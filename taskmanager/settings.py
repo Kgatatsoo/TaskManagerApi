08b3b40d5b71f86ce2825ce1f411f125a146f6f7
kgatatsoo <vibrationbanda@gmail.com>
2026-01-03 13:58:31 +0200

    Setup for urls and installed apps

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'corsheaders',
+    'taskmanagerapp',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.corsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOW_ALL_ORIGIN = True
+ 
+ REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        ],
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.ISAuthenticated',
+        ]
+    
+ }
