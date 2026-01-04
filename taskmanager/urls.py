b64c0c7fe8d4f7b4ac7783419e94d7f685dd709d
kgatatsoo <vibrationbanda@gmail.com>
2026-01-04 20:13:14 +0200

    added Procfile for deployment

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),  # this is fine, but check spelling/capitalization
+    path('auth/', include('djoser.urls.authtoken')),  
     path('api/', include('taskmanagerapp.urls')),
 ]
